050eec8f8424bd9316d321b2f1db3069312c08e5
Adnan Lahrech <lahadn@gmail.com>
2022-08-19 21:14:57 +0100

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:adnanlah/rotsprite-webgl.git main:gh-pages
+
+cd -

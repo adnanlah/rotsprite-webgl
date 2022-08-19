561a4fa28871b9a3739bd2b4265f8c2522dbdc12
Adnan Lahrech <lahadn@gmail.com>
2022-08-19 21:50:03 +0100

    removed outdir

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/rotsprite-webgl/',
-  build: {
-    outDir: '/docs',
-  },
   plugins: [react()],
 })

2b73384a402a8df78c0a8cf1e36383d72b96b91a
Adnan Lahrech <lahadn@gmail.com>
2022-08-20 11:45:29 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git config --global user.email "lahadn@gmail.com"
 git config --global user.name "Adnan Lahrech"
 
 # git config --global url."git@github.com:".insteadOf "https://github.com/"
-git push -f https://github.com/adnanlah/rotsprite-webgl.git main:gh-pages
-# git push -f git@github.com:adnanlah/rotsprite-webgl.git main:gh-pages
+# git push -f https://github.com/adnanlah/rotsprite-webgl.git main:gh-pages
+git push -f git@github.com:adnanlah/rotsprite-webgl.git main:gh-pages
 
 cd -
